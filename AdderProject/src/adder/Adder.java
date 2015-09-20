a60138d0122657e4cb95c819fa779995fd81d56e
alan120364 <alan120364@yahoo.com.hk>
2015-09-20 11:16:31 +0800

    1st commit

---
+++ AdderProject/src/adder/Adder.java
@@ -0,0 +1,15 @@
+package adder;
+
+public class Adder {
+	
+	/**
+	 * Add2 integer.
+	 *
+	 * @param a the 1st integer input
+	 * @param b the 2nd integer input
+	 * @return the sum of a and b as an integer
+	 */
+	public int add2Integer(int a, int b){
+		return a + b;
+	}
+}
---
+++ AdderProject/src/adderTest/AdderTest.java
@@ -0,0 +1,14 @@
+package adderTest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AdderTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
