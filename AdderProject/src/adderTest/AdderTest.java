c4dfc3dda8d84fb24a9342fc1ac5e7d94a9db874
alan120364 <alan120364@yahoo.com.hk>
2015-09-20 11:28:57 +0800

    Added doc description to AdderTest. - Alan

--- AdderProject/src/adderTest/AdderTest.java
+++ AdderProject/src/adderTest/AdderTest.java
@@ -4,8 +4,15 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Class AdderTest.
+ */
 public class AdderTest {
 
+	/**
+	 * Test.
+	 */
 	@Test
 	public void test() {
 		fail("Not yet implemented");
