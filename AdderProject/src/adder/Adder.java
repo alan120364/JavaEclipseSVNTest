3c2efcebe767e6ce5e5e28652bb48dcd38541ffb
g10932790 <g10932790@trbvm.com>
2015-09-20 12:24:22 +0800

    add the doc to the newly added method add3Integer() and edit the doc of the add2Integer() - User2

--- AdderProject/src/adder/Adder.java
+++ AdderProject/src/adder/Adder.java
@@ -13,13 +13,21 @@ public class Adder {
 	 *
 	 * @param a the 1st integer input
 	 * @param b the 2nd integer input
-	 * @return the sum of a and b as an integer
+	 * @return the sum
 	 */
 	public int add2Integer(int a, int b){
 		return a + b;
 	}
 	
 	
+	/**
+	 * Add3 integer.
+	 *
+	 * @param a the 1st integer input
+	 * @param b the 2nd integer input
+	 * @param c the 3rd integer input
+	 * @return the sum
+	 */
 	public int add3Integer(int a, int b, int c){
 		return a + b + c;
 	}
